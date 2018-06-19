ef297ec9949703e5b3f356ac5a9fe0759d1cd1a6
HIKALU <play3a@126.com>
2018-06-19 20:56:30 +0800

    p8

--- src/practice_8/class.js
+++ src/practice_8/class.js
@@ -1,1 +1,15 @@
+class Class {
+    constructor(number) {
+        this.number = number;
+    }
 
+    getDisplayName() {
+        return "Class " + this.number;
+    }
+    assignLeader(student) {
+        if (this === student.klass) {
+            this.leader = student;
+        }
+    }
+}
+export default Class;
